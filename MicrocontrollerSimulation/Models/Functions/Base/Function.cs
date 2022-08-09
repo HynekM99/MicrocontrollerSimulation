e25597cf4d1eb76b36d462a6917d59337a428db4
SagvanWeird <hynekmoudry@gmail.com>
2022-08-09 11:53:26 +0200

    Added Functions and their collection.

---
+++ MicrocontrollerSimulation/Models/Functions/Base/Function.cs
@@ -0,0 +1,26 @@
+﻿using MicrocontrollerSimulation.Models.LogicalExpressions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrocontrollerSimulation.Models.Functions.Base
+{
+    public class Function
+    {
+        public string Name { get; }
+        public LogicalExpression Expression { get; }
+
+        public Function(string name, LogicalExpression expression)
+        {
+            Name = name;
+            Expression = expression;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ MicrocontrollerSimulation/Models/Functions/Collections/FunctionsCollection.cs
@@ -0,0 +1,28 @@
+﻿using MicrocontrollerSimulation.Models.Functions.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrocontrollerSimulation.Models.Functions.Collections
+{
+    public class FunctionsCollection : ObservableCollection<Function>
+    {
+        public new void Add(Function function)
+        {
+            if (this.Any(f => f.Name == function.Name))
+            {
+                throw new ArgumentException($"An expression with the same name ({function}) is already in the list.");
+            }
+
+            base.Add(function);
+        }
+
+        public Function? Find(string name)
+        {
+            return this.Where(function => function.Name == name).FirstOrDefault();
+        }
+    }
+}
