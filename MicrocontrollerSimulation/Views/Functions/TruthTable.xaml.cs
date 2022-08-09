684a5232c05bea29fe7dab83292cca2de62ee038
SagvanWeird <hynekmoudry@gmail.com>
2022-08-09 20:22:48 +0200

    Added a truth table view.

---
+++ MicrocontrollerSimulation/Views/Functions/TruthTable.xaml.cs
@@ -0,0 +1,150 @@
+﻿using MicrocontrollerSimulation.Models.Functions.Base;
+using MicrocontrollerSimulation.Models.LogicalExpressions.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicrocontrollerSimulation.Views.Functions
+{
+    /// <summary>
+    /// Interakční logika pro TruthTable.xaml
+    /// </summary>
+    public partial class TruthTable : UserControl
+    {
+        #region FunctionDependencyProperty
+        public Function? Function
+        {
+            get => (Function)GetValue(FunctionProperty);
+            set => SetValue(FunctionProperty, value);
+        }
+
+        public static readonly DependencyProperty FunctionProperty = DependencyProperty.Register(nameof(Function), typeof(Function), typeof(TruthTable), new PropertyMetadata(null, OnExpressionChanged));
+
+        private static void OnExpressionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var truthTable = (TruthTable)d;
+            truthTable.ClearTable();
+
+            if (e.NewValue is null) return;
+
+            Function newValue = (Function)e.NewValue;
+            List<Input> inputs = newValue.Expression.Inputs.ToList();
+
+            truthTable.SetupGrid(inputs);
+            truthTable.SetHeaders(inputs);
+            truthTable.SetRows(inputs);
+        }
+        #endregion
+
+        public TruthTable()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearTable()
+        {
+            mainGrid.ColumnDefinitions.Clear();
+            mainGrid.RowDefinitions.Clear();
+            mainGrid.Children.Clear();
+        }
+
+        private void SetupGrid(List<Input> inputs)
+        {
+            int columns = inputs.Count + 2;
+            int rows = (1 << inputs.Count) + 2;
+
+            for (int i = 0; i < columns; i++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = GridLength.Auto
+                });
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = GridLength.Auto
+                });
+            }
+
+            Rectangle topLine = new()
+            {
+                Height = 1,
+                Fill = new SolidColorBrush(Colors.Black)
+            };
+            Grid.SetRow(topLine, 1);
+            Grid.SetColumnSpan(topLine, columns);
+
+            Rectangle rightLine = new()
+            {
+                Width = 1,
+                Fill = new SolidColorBrush(Colors.Black)
+            };
+            Grid.SetColumn(rightLine, inputs.Count);
+            Grid.SetRowSpan(rightLine, rows);
+
+            mainGrid.Children.Add(topLine);
+            mainGrid.Children.Add(rightLine);
+        }
+
+        private void SetHeaders(List<Input> inputs)
+        {
+            if (Function is null) return;
+
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                Input input = inputs[i];
+                AddTextBlock(input.ToString(), 0, i);
+            }
+            AddTextBlock(Function.Name, 0, inputs.Count + 1);
+        }
+
+        private void SetRows(List<Input> inputs)
+        {
+            if (Function is null) return;
+
+            int columns = inputs.Count;
+            int rows = 1 << inputs.Count;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    bool bit = (i & (1 << (columns - j - 1))) > 0;
+
+                    inputs[j].Value = bit;
+                    AddTextBlock((bit ? 1 : 0).ToString(), i + 2, j);
+                }
+
+                AddTextBlock((Function.Expression.Result ? 1 : 0).ToString(), i + 2, columns + 1);
+            }
+        }
+
+        private void AddTextBlock(string text, int row, int column)
+        {
+            TextBlock tb = new()
+            {
+                Text = text,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(4, 0, 4, 0)
+            };
+            Grid.SetColumn(tb, column);
+            Grid.SetRow(tb, row);
+
+            mainGrid.Children.Add(tb);
+        }
+    }
+}
