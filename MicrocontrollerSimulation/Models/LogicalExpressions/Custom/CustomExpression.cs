a767b703680089c899b257256799eadfb7fe63b1
SagvanWeird <hynekmoudry@gmail.com>
2022-08-10 18:58:12 +0200

    Fixed namespace in CustomExpression.

--- MicrocontrollerSimulation/Models/LogicalExpressions/Custom/CustomExpression.cs
+++ MicrocontrollerSimulation/Models/LogicalExpressions/Custom/CustomExpression.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MCUSimulation.Models.LogicalExpressions.Custom
+namespace MicrocontrollerSimulation.Models.LogicalExpressions.Custom
 {
     public class CustomExpression : LogicalExpression
     {
