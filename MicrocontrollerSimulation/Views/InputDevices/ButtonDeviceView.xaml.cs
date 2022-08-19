53e47db877e71a5a0e306e2d9e0b965931262bd1
SagvanWeird <hynekmoudry@gmail.com>
2022-08-19 08:33:11 +0200

    Added input devices views.

---
+++ MicrocontrollerSimulation/Views/InputDevices/ButtonDeviceView.xaml.cs
@@ -0,0 +1,85 @@
+﻿using MicrocontrollerSimulation.Models.InputDevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicrocontrollerSimulation.Views.InputDevices
+{
+    /// <summary>
+    /// Interakční logika pro ButtonDeviceView.xaml
+    /// </summary>
+    public partial class ButtonDeviceView : UserControl
+    {
+        public bool IsPressed
+        {
+            get { return (bool)GetValue(IsPressedProperty); }
+            set { SetValue(IsPressedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsPressedProperty = DependencyProperty.Register(
+                nameof(IsPressed), typeof(bool), typeof(ButtonDeviceView), 
+                new PropertyMetadata(false));
+
+        public ButtonDeviceView()
+        {
+            InitializeComponent();
+
+            MouseLeftButtonDown += OnMouseLeftButtonDown;
+            MouseLeave += OnMouseLeave;
+            MouseLeftButtonUp += OnMouseLeftButtonUp;
+            KeyDown += OnKeyDown;
+            KeyUp += OnKeyUp;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter ||
+                e.Key == Key.Space)
+            {
+                SetButtonState(true);
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter ||
+                e.Key == Key.Space)
+            {
+                SetButtonState(false);
+            }
+        }
+
+        private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            SetButtonState(false);
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            SetButtonState(false);
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SetButtonState(true);
+        }
+
+        private void SetButtonState(bool pressed)
+        {
+            IsPressed = pressed;
+            ButtonPressedTemplate.Visibility = pressed ? Visibility.Visible : Visibility.Collapsed;
+            ButtonNotPressedTemplate.Visibility = !pressed ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ MicrocontrollerSimulation/Views/InputDevices/SwitchDeviceView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicrocontrollerSimulation.Views.InputDevices
+{
+    /// <summary>
+    /// Interakční logika pro SwitchDeviceView.xaml
+    /// </summary>
+    public partial class SwitchDeviceView : UserControl
+    {
+        public bool IsToggled
+        {
+            get { return (bool)GetValue(IsToggledProperty); }
+            set { SetValue(IsToggledProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsToggledProperty = DependencyProperty.Register(
+                nameof(IsToggled), typeof(bool), typeof(SwitchDeviceView),
+                new PropertyMetadata(false));
+
+        public SwitchDeviceView()
+        {
+            InitializeComponent();
+
+            MainButton.Click += MainButton_Click;
+        }
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitchState();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter ||
+                e.Key == Key.Space)
+            {
+                ToggleSwitchState();
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter ||
+                e.Key == Key.Space)
+            {
+                ToggleSwitchState();
+            }
+        }
+
+        private void ToggleSwitchState()
+        {
+            IsToggled = !IsToggled;
+            SwitchOnTemplate.Visibility = IsToggled ? Visibility.Visible : Visibility.Collapsed;
+            SwitchOffTemplate.Visibility = !IsToggled ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
