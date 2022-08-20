6c5dc35401a0b1413eedd8f788f6a261895fdea7
SagvanWeird <hynekmoudry@gmail.com>
2022-08-20 18:54:32 +0200

    Added more default functions to new_project.

--- MicrocontrollerSimulation/Models/Project/ProjectInfo.cs
+++ MicrocontrollerSimulation/Models/Project/ProjectInfo.cs
@@ -46,20 +46,39 @@ namespace MicrocontrollerSimulation.Models.Project
         {
             FunctionsCollection defaultFunctions = new();
 
-            Not not = new(new Input("IN"));
-            And and = new(new Input("A"), new Input("B"));
-            Or or = new(new Input("A"), new Input("B"));
-            Xor xor = new(new Input("A"), new Input("B"));
+            var not = new Not(new Input("IN"));
+            var and = new And(new Input("A"), new Input("B"));
+            var or = new Or(new Input("A"), new Input("B"));
+            var xor = new Xor(new Input("A"), new Input("B"));
+
+            var nand = new Not(new And(new Input("A"), new Input("B")));
+            var nor = new Not(new Or(new Input("A"), new Input("B")));
+            var xnor = new Not(new Xor(new Input("A"), new Input("B")));
 
             var notFunction = new Function("Not", not);
             var andFunction = new Function("And", and);
             var orFunction = new Function("Or", or);
             var xorFunction = new Function("Xor", xor);
 
+            var nandFunction = new Function("Nand", nand);
+            var norFunction = new Function("Nor", nor);
+            var xnorFunction = new Function("Xnor", xnor);
+
+            var srAnd = new And(new Input("Q"), new Not(new Input("R")));
+            var srOr = new Or(new Input("S"), srAnd);
+
+            var srFunction = new Function("SR_Latch", srOr);
+
             defaultFunctions.Add(notFunction);
             defaultFunctions.Add(andFunction);
             defaultFunctions.Add(orFunction);
             defaultFunctions.Add(xorFunction);
+
+            defaultFunctions.Add(nandFunction);
+            defaultFunctions.Add(norFunction);
+            defaultFunctions.Add(xnorFunction);
+
+            defaultFunctions.Add(srFunction);
 
             return new ProjectInfo(
                 name,
