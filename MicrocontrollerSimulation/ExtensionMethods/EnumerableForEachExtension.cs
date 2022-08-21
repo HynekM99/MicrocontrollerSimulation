79019a21961233335a1f26c0c8f3d201ba2f5916
SagvanWeird <hynekmoudry@gmail.com>
2022-08-21 17:20:30 +0200

    Added and extension method for IEnumerable ForEach.

---
+++ MicrocontrollerSimulation/ExtensionMethods/EnumerableForEachExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrocontrollerSimulation.ExtensionMethods
+{
+    public static class EnumerableForEachExtension
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+
+            return enumerable;
+        }
+    }
+}
