940aae79ed0f4ce03cfa8865e8df4fbb099205c8
SagvanWeird <hynekmoudry@gmail.com>
2022-08-22 10:11:36 +0200

    User can now copy a function's expression.

--- MicrocontrollerSimulation/Views/Functions/SelectedFunctionView.xaml.cs
+++ MicrocontrollerSimulation/Views/Functions/SelectedFunctionView.xaml.cs
@@ -24,5 +24,10 @@ namespace MicrocontrollerSimulation.Views.Functions
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(ExpressionTextBox.Text);
+        }
     }
 }
